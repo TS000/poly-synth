468a893ba84701c417815a0c2af01999acdc7f78
TS000 <tylersnell@me.com>
2021-07-28 20:35:14 -0700

    beginning of polyness

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const core = require('elementary-core');
 const el = require('@nick-thompson/elementary');
+const path = require('path');
+
+let voices = [
+    { gate: 0.0, freq: 440, 'v1' },
+    { gate: 0.0, freq: 440, 'v2' },
+    { gate: 0.0, freq: 440, 'v3' },
+    { gate: 0.0, freq: 440, 'v4' },
+];
+
+let nextVoice = 0;
+
+function synthVoice(voice) {
+    return el.mul(0.2 * voice.gate, el.cycle(voice.freq));
+}
 
 core.on('load', function() {
     setInterval(function(){
